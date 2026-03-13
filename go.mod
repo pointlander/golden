7feb0d79b3f43752cc83b01b48988089a01223f9
Andrew Snodgrass <pointlander@gmail.com>
2026-03-13 18:37:35 -0500

    Added golden model

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/pointlander/golden
 
 go 1.25.0
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pointlander/gradient v0.0.0-20260309183805-29228c81c690 // indirect
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -9,8 +9,16 @@ import (
 	"bytes"
 	"embed"
 	"encoding/csv"
+	"fmt"
 	"io"
+	"math"
+	"math/rand"
+	"sort"
 	"strconv"
+	"strings"
+
+	"github.com/pointlander/golden/kmeans"
+	"github.com/pointlander/gradient/tf64"
 )
 
 //go:embed iris.zip
@@ -38,6 +46,162 @@ var Inverse = [4]string{
 	"Iris-setosa",
 	"Iris-versicolor",
 	"Iris-virginica",
+}
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = 1.0e-2
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
+)
+
+// LearnEmbeddingIris learns the iris embedding
+func LearnEmbeddingIris(iris []Fisher, size, iterations int) []Fisher {
+	rng := rand.New(rand.NewSource(1))
+	others := tf64.NewSet()
+	length := len(iris)
+	cp := make([]Fisher, length)
+	copy(cp, iris)
+	others.Add("b", size, len(cp))
+	x := others.ByName["b"]
+	for _, row := range iris {
+		x.X = append(x.X, row.Measures...)
+	}
+
+	set := tf64.NewSet()
+	set.Add("a", size, len(cp))
+
+	for ii := range set.Weights {
+		w := set.Weights[ii]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float64, StateTotal)
+			for ii := range w.States {
+				w.States[ii] = make([]float64, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for range cap(w.X) {
+			w.X = append(w.X, rng.NormFloat64()*factor)
+		}
+		w.States = make([][]float64, StateTotal)
+		for ii := range w.States {
+			w.States[ii] = make([]float64, len(w.X))
+		}
+	}
+
+	l0 := tf64.Add(tf64.Mul(others.Get("b"), set.Get("a")), tf64.Square(others.Get("b")))
+	loss := tf64.Avg(tf64.Quadratic(tf64.Square(set.Get("a")), l0))
+
+	for iteration := range iterations {
+		pow := func(x float64) float64 {
+			y := math.Pow(x, float64(iteration+1))
+			if math.IsNaN(y) || math.IsInf(y, 0) {
+				return 0
+			}
+			return y
+		}
+
+		set.Zero()
+		others.Zero()
+		l := tf64.Gradient(loss).X[0]
+		if math.IsNaN(float64(l)) || math.IsInf(float64(l), 0) {
+			fmt.Println(iteration, l)
+			return nil
+		}
+
+		norm := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = math.Sqrt(norm)
+		b1, b2 := pow(B1), pow(B2)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+		for _, w := range set.Weights {
+			for ii, d := range w.D {
+				g := d * scaling
+				m := B1*w.States[StateM][ii] + (1-B1)*g
+				v := B2*w.States[StateV][ii] + (1-B2)*g*g
+				w.States[StateM][ii] = m
+				w.States[StateV][ii] = v
+				mhat := m / (1 - b1)
+				vhat := v / (1 - b2)
+				if vhat < 0 {
+					vhat = 0
+				}
+				_ = mhat
+				w.X[ii] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+			}
+		}
+		fmt.Println(l)
+	}
+
+	meta := make([][]float64, len(cp))
+	for i := range meta {
+		meta[i] = make([]float64, len(cp))
+	}
+	const k = 3
+
+	{
+		y := set.ByName["a"]
+		vectors := make([][]float64, len(cp))
+		for i := range vectors {
+			row := make([]float64, size)
+			for ii := range row {
+				row[ii] = y.X[i*size+ii]
+			}
+			vectors[i] = row
+		}
+		for i := 0; i < 33; i++ {
+			clusters, _, err := kmeans.Kmeans(int64(i+1), vectors, k, kmeans.SquaredEuclideanDistance, -1)
+			if err != nil {
+				panic(err)
+			}
+			for i := 0; i < len(meta); i++ {
+				target := clusters[i]
+				for j, v := range clusters {
+					if v == target {
+						meta[i][j]++
+					}
+				}
+			}
+		}
+	}
+	clusters, _, err := kmeans.Kmeans(1, meta, 3, kmeans.SquaredEuclideanDistance, -1)
+	if err != nil {
+		panic(err)
+	}
+	for i := range clusters {
+		cp[i].Cluster = clusters[i]
+	}
+	for _, value := range x.X[len(iris)*size:] {
+		cp[len(iris)].Measures = append(cp[len(iris)].Measures, value)
+	}
+	I := set.ByName["a"]
+	for i := range cp {
+		cp[i].Embedding = I.X[i*size : (i+1)*size]
+	}
+	sort.Slice(cp, func(i, j int) bool {
+		return cp[i].Cluster < cp[j].Cluster
+	})
+	return cp
 }
 
 // Load loads the iris data set
@@ -91,5 +255,16 @@ func Load() []Fisher {
 }
 
 func main() {
-	Load()
+	iris := Load()
+	cp := LearnEmbeddingIris(iris, 4, 2*1024)
+	acc := make(map[string][3]int)
+	for i := range cp {
+		fmt.Println(cp[i].Cluster, cp[i].Label)
+		counts := acc[cp[i].Label]
+		counts[cp[i].Cluster]++
+		acc[cp[i].Label] = counts
+	}
+	for i, v := range acc {
+		fmt.Println(i, v)
+	}
 }
